fa352b7b8df59918494737a23c4cdb52b1f997d7
franpais <juanfranpais02@gmail.com>
2022-02-03 20:20:50 -0300

    heroku

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.listen(2500);
+app.listen(process.env.PORT || 2500, function() {
+    console.log("server corriendo en puerto 2500")
+});
 
 const publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
